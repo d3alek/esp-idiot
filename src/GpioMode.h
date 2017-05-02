6fb5572d1bad04a6b9e37a8623588605b92835fa
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2017-05-02 21:24:49 +0300

    Changing pin labels to reflect hardware

--- src/GpioMode.h
+++ src/GpioMode.h
@@ -4,10 +4,10 @@
 #define MAX_GPIO_MODES 6
 #define AUTO 2
 
-#define PIN_A 4
+#define PIN_A 13
 #define PIN_B 5
-#define PIN_C 12
-#define PIN_D 13
+#define PIN_C 4
+#define PIN_D 12
 #define PIN_E 14
 #define PIN_F 16
 
